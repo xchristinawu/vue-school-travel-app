6f188f4c67ffa07696fc28ec232ea7f61f2613a3
Christina Wu <xchristinawu@gmail.com>
2023-09-14 20:26:23 -0700

    add home and about pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
- 
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link> |
+  <router-view></router-view>
 </template>
 
 <script >
@@ -8,16 +10,4 @@
 
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import { createApp } from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {path: '/', name: 'Home', component: Home},
+        {path: '/about', name: 'About', component: About}
+    ]
 })
 
 createApp(App)
+.use(router)
 .mount('#app')
