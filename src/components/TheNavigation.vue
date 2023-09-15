fcd282b561c70cdb49948c431e935749928015f7
Christina Wu <xchristinawu@gmail.com>
2023-09-15 10:55:13 -0700

    create dynamic routes

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,16 +1,13 @@
 <template>
-    <div class="home">
-        <h1>All Destinations</h1>
-        <div class="destinations">
-            <router-link 
+    <div id="nav">
+        <router-link id="logo" to="/">Travel App</router-link>
+        <router-link 
             v-for="destination in destinations"
             :key="destination.id"
             :to="{name: 'destination.show', params: {id: destination.id, slug: destination.slug}}"
-            >
-            <h2>{{ destination.name }}</h2>
-            <img :src="`/images/${destination.image}`" :alt="destination.name">
-            </router-link>
-        </div>
+        >
+            {{ destination.name }}
+        </router-link>
     </div>
 </template>
 
