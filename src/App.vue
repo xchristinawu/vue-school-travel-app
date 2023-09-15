c8462b7b7daac804fffe23252b4372a870dfc88a
Christina Wu <xchristinawu@gmail.com>
2023-09-14 21:31:31 -0700

    add destinations

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link> |
-  <router-view></router-view>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script >
 
 </script>
-
-
-<style scoped>
-</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import { createApp } from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import './style.css'
 import App from './App.vue'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path: '/', name: 'Home', component: Home},
-        {path: '/about', name: 'About', component: About}
-    ]
-})
+import router from '@/router'
 
 createApp(App)
 .use(router)
