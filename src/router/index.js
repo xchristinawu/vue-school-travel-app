f477095385507052332de38151ac345f63c90623
Christina Wu <xchristinawu@gmail.com>
2023-09-15 23:14:53 -0700

    add experiences view and component

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,18 @@ import Home from '@/views/Home.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
-    {path: '/destination/:id/:slug', name: 'destination.show', component: ()=>import('@/views/DestinationShow.vue')}
+    {
+        path: '/destination/:id/:slug', 
+        name: 'destination.show', 
+        component: () => import('@/views/DestinationShow.vue'),
+        props: route => ({...route.params, id: parseInt(route.params.id)})
+    },
+    {
+        path: `/destination/:id/:slug/:experienceSlug`,
+        name: `experience.show`,
+        component: () => import('@/views/ExperienceShow.vue'),
+        props: route => ({...route.params, id: parseInt(route.params.id)})
+    }
 ]
 
 const router = createRouter({
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -6,18 +6,36 @@
       <p>{{destination.description}}</p>
     </div>
   </section>
+  <section class="experiences">
+    <h2>Top Experiences in {{ destination.name }}</h2>
+    <div class="cards">
+        <router-link
+            v-for="experience in destination.experiences"
+            :key="experience.slug"
+            :to="{name: 'experience.show', params:{experienceSlug: experience.slug}}"
+        >
+            <ExperienceCard 
+                :experience="experience"
+            />
+        </router-link>
+    </div>
+    
+  </section>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import ExperienceCard from '@/components/ExperienceCard.vue'
+
 export default {
+    components:{ExperienceCard},
+    props:{
+        id: {type: Number, required: true}
+    },
     computed:{
-        destinationId(){
-            return parseInt(this.$route.params.id)
-        },
         destination(){
-            return sourceData.destinations.find(destination => destination.id === this.destinationId)
+            return sourceData.destinations.find(destination => destination.id === this.id)
         }
-    }
+    },
 }
 </script>
