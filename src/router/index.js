54f0592e9bef1fee7de191ad62958db6f2eed9ed
Christina Wu <xchristinawu@gmail.com>
2023-09-28 20:16:15 -0700

    add 404 not found page & route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '@/views/Home.vue'
+import sourceData from '@/data.json'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
@@ -8,6 +9,18 @@ const routes = [
         name: 'destination.show',
         component: () => import('@/views/DestinationShow.vue'),
         props: route => ({...route.params, id: parseInt(route.params.id)}),
+        beforeEnter(to, from) {
+            const exists = sourceData.destinations.find(
+                destination => destination.id === parseInt(to.params.id)
+            )
+            if(!exists) return {
+                name: 'NotFound',
+                // allows keeping the URL while rendering a different page
+                params: { pathMatch: to.path.split('/').slice(1) },
+                query: to.query,
+                hash: to.hash,
+            }
+        },
         children: [
             {
                 path: ':experienceSlug',
@@ -17,6 +30,11 @@ const routes = [
             }
         ]
     },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () => import('@/views/NotFound.vue')
+    }
 ]
 
 const router = createRouter({
